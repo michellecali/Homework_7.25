9e607475ec49289cd9ba87c79c23d68a8b314b38
Michelle Cali <mctexx@gmail.com>
2016-07-26 16:42:09 -0500

    not reading JSON

--- Quiz_PHP/check_answer.php
+++ Quiz_PHP/check_answer.php
@@ -1,21 +1,32 @@
 <?php
+	
+	$string = $_GET['string'];
+	$array = explode(",", $string);
+	$q1Result = "incorrect";
+	$q2Result = "incorrect";
+	$q3Result = "incorrect";
+	if ($array[0] == "Blue") {
+		$q1Result = "correct";
+		return $q1Result;
+	}
+	if ($array[1] == "White") {
+		$q2Result = "correct";
+		return $q2Result;
+	}
+	if ($array[2] == "Brown") {
+		$q3Result = "correct";
+		return $q3Result;
+	}
 
-	if ($_GET['string'] == "Blue") {
 
-?>
+	
+	$post_data = array(
+    'q1' => $q1Result,
+    'q2' => $q2Result,
+    'q3' => $q3Result
+  	);
 
-{"q1": "correct"}
 
-<?php
-
-	} else {
-
-?>
-
-{"q1": "pizzaface"}
-
-<?php
-
-}
-
+echo json_encode($post_data);
+	 
 ?>
--- Quiz_PHP/index.php
+++ Quiz_PHP/index.php
@@ -11,6 +11,18 @@
 <div class="questions" id="q1" style="display: none">
 	<h1>What color is the sky?</h1>
 	<input id="a1" type="text" class="answers" name="a1">
+	<button class="newQ" id="1" name="nextQ">Next Question</button>
+</div> 
+
+<div class="questions" id="q2" style="display: none">
+	<h1>What color are the clouds?</h1>
+	<input id="a2" type="text" class="answers" name="a2">
+	<button class="newQ" id="2" name="nextQ">Next Question</button>
+</div> 
+
+<div class="questions" id="q3" style="display: none">
+	<h1>What color is the mud?</h1>
+	<input id="a3" type="text" class="answers" name="a3">
 	<button name="getResults" id="getResults">Get results</button>
 </div> 
 
@@ -18,6 +30,8 @@
 
 <div class="results">
 	<span id="r1"></span>
+	<span id="r2"></span>
+	<span id="r3"></span>
 </div>
 
 
