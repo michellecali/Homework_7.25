1b9882c53ac4fcb399a9cd5453dec0aa106b1d5d
Michelle Cali <mctexx@gmail.com>
2016-07-27 11:31:01 -0500

    finished

--- Quiz_PHP/check_answer.php
+++ Quiz_PHP/check_answer.php
@@ -1,29 +1,23 @@
 <?php
 	
-	
-	$string = "Blue,White,Brown";
 
-	// $string = $_GET['string'];
+	$string = $_GET['string'];
 	$array = explode(",", $string);
-	// print_r($array);
 
 	$q1Result = "incorrect";
 	if ($array[0] == "Blue") {
 		$q1Result = "correct";
 	}
-	// echo $q1Result;
 
 	$q2Result = "incorrect";
 	if ($array[1] == "White") {
 		$q2Result = "correct";
 	}
-	// echo $q2Result;
 
 	$q3Result = "incorrect";
 	if ($array[2] == "Brown") {
 		$q3Result = "correct";
 	}
-	// echo $q3Result;
 
 	
 	$post_data = array(
@@ -31,8 +25,6 @@
     'q2' => $q2Result,
     'q3' => $q3Result
   	);
-
-// echo $post_data;
 
 echo json_encode($post_data);
 	 
